6d27525f0f686f1269b5f27cc4692be64012c1f7
mathycreater <46695762+mathycreater@users.noreply.github.com>
2020-02-28 09:50:54 -0300

    Add files via upload

---
+++ Python_Avançado.py
@@ -0,0 +1,8 @@
+x = [1, 2, 3, 4, 5]
+y = []
+
+for i in x:
+    y.append(i**2)
+
+print(x)
+print(y)
---
+++ equação.py
@@ -0,0 +1,10 @@
+import math
+
+a = 4
+b = 5
+c = 4
+
+def delta(a,b,c):
+  delta = (b**2) - (4*a*c)
+  print(delta)
+  return {"delta":delta}
